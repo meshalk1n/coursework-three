ae983f3de0d603f2f98c5473e1f85bb2227d8cea
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2023-12-19 16:20:06 +0300

    feat: успешное подключение к базе данных

--- src/main/java/com/example/models/Estate.java
+++ src/main/java/com/example/models/Estate.java
@@ -10,7 +10,7 @@ public class Estate {
 
     @Id
     @Column(name = "id")
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     @Column(name = "name")
